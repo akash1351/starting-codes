29852581ea048d4c79bbe5131a9a13de6b56710f
akash1351 <81347485+akash1351@users.noreply.github.com>
2022-07-05 23:11:54 +0530

    Add files via upload

---
+++ datatype size.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+char b;
+float c;
+bool d;
+long e;
+double f;
+cout<<"size of a "<<sizeof(a)<<endl;
+cout<<"size of b "<<sizeof(b)<<endl;
+cout<<"size of c "<<sizeof(c)<<endl;
+cout<<"size of d "<<sizeof(d)<<endl;
+cout<<"size of e "<<sizeof(e)<<endl;
+cout<<"size of f "<<sizeof(f)<<endl;
+
+    return 0;
+}
